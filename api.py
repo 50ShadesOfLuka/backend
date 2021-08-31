19f1167ca6b63c74df37444a1ce0ce84888bb0aa
Luka <355880@student.fontys.nl>
2021-08-31 15:42:58 +0200

    Present data as json

--- api.py
+++ api.py
@@ -2,7 +2,11 @@ import datetime   # This will be needed later
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from bson import json_util
 import json
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
 
 # Load config from a .env file:
 load_dotenv()
@@ -18,6 +22,24 @@ collection = db['epeldata']
 #     file_data = json.load(f)
 # collection.insert_many(file_data)
 
-data = list(collection.find())
+def getData():
+    data = list(collection.find())
+    data = {item['id']:item for item in data}
+    return data
 
-print(data)
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        pass
+    else:
+        data = getData()
+        data = parse_json(data)
+        return data 
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+    
